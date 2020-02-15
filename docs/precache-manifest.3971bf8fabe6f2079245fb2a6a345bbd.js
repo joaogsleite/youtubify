b09c5f4619b2d92771a5890502d4811e35f010bd
joaogsleite <joao.leite@isobar.com>
2020-02-15 17:33:30 +0000

    github pages

---
+++ docs/precache-manifest.3971bf8fabe6f2079245fb2a6a345bbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cfcfc2ffa9390d2a63db6fc3517a555",
+    "url": "/youtubify/index.html"
+  },
+  {
+    "revision": "d232b5c79602fef6bcd9",
+    "url": "/youtubify/static/css/main.b0b02bc1.chunk.css"
+  },
+  {
+    "revision": "e92a172a63fbce7e5585",
+    "url": "/youtubify/static/js/2.870281d2.chunk.js"
+  },
+  {
+    "revision": "496630de2713ba70683da5dc3fc3c1a4",
+    "url": "/youtubify/static/js/2.870281d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d232b5c79602fef6bcd9",
+    "url": "/youtubify/static/js/main.605ae590.chunk.js"
+  },
+  {
+    "revision": "c64f7bcc5eca90bd42d3",
+    "url": "/youtubify/static/js/runtime-main.c39d4d6a.js"
+  }
+]);
