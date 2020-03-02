59a91773c74fb104c37dd7367e44946f300413dc
joaogsleite <joaogsleite@users.noreply.github.com>
2020-03-02 17:44:58 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.42a6e48089b2c8b5f81a5963ebb6a558.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e941e878fa2faca191f096ed0f399c7c",
+    "url": "/youtubify/index.html"
+  },
+  {
+    "revision": "0f338dd73f970a316baa",
+    "url": "/youtubify/static/css/main.5cbe8f36.chunk.css"
+  },
+  {
+    "revision": "2882330c6118fcfaeb56",
+    "url": "/youtubify/static/js/2.c8ac5b07.chunk.js"
+  },
+  {
+    "revision": "766f5467ea3a1c4c55812f3dcfefe31d",
+    "url": "/youtubify/static/js/2.c8ac5b07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f338dd73f970a316baa",
+    "url": "/youtubify/static/js/main.012f1fdf.chunk.js"
+  },
+  {
+    "revision": "0ab01d938247c151f376",
+    "url": "/youtubify/static/js/runtime-main.70ec9f96.js"
+  },
+  {
+    "revision": "0037347f16a6dd10447b0b2911b0104c",
+    "url": "/youtubify/static/media/fa-light-300.0037347f.ttf"
+  },
+  {
+    "revision": "86683af0360cc024c998f00de4c43e9e",
+    "url": "/youtubify/static/media/fa-light-300.86683af0.woff2"
+  },
+  {
+    "revision": "97468fa17b539584bbe0b351319d66e7",
+    "url": "/youtubify/static/media/fa-light-300.97468fa1.eot"
+  },
+  {
+    "revision": "bd79c4a8d73aa9c8d417cf2ad1e92e25",
+    "url": "/youtubify/static/media/fa-light-300.bd79c4a8.svg"
+  },
+  {
+    "revision": "d8e14023b11ab348bcd6148a50b7ae85",
+    "url": "/youtubify/static/media/fa-light-300.d8e14023.woff"
+  },
+  {
+    "revision": "39bd303fbf945408f7f6e585bc086ee4",
+    "url": "/youtubify/static/media/fa-regular-400.39bd303f.ttf"
+  },
+  {
+    "revision": "75721d809c7b38cce914a6bd07b0973b",
+    "url": "/youtubify/static/media/fa-regular-400.75721d80.eot"
+  },
+  {
+    "revision": "983a20d28fbf8fbc27cfeb096f0ebd85",
+    "url": "/youtubify/static/media/fa-regular-400.983a20d2.woff2"
+  },
+  {
+    "revision": "995e3bb44c723366ac51798168a74e71",
+    "url": "/youtubify/static/media/fa-regular-400.995e3bb4.svg"
+  },
+  {
+    "revision": "dd012cb958a95fc77ced4ecbb7c87040",
+    "url": "/youtubify/static/media/fa-regular-400.dd012cb9.woff"
+  },
+  {
+    "revision": "265b724844b97d7427fc4a44c9c2e43d",
+    "url": "/youtubify/static/media/fa-solid-900.265b7248.woff"
+  },
+  {
+    "revision": "27df36c7e2bec8e3624bed94f02e7aa8",
+    "url": "/youtubify/static/media/fa-solid-900.27df36c7.ttf"
+  },
+  {
+    "revision": "464959304adf524aa8e6ba9bd3512a05",
+    "url": "/youtubify/static/media/fa-solid-900.46495930.svg"
+  },
+  {
+    "revision": "9b39dfc9bafe5944979aff454bdfe44f",
+    "url": "/youtubify/static/media/fa-solid-900.9b39dfc9.eot"
+  },
+  {
+    "revision": "e245d7d62db89a9912e17b02bc23e256",
+    "url": "/youtubify/static/media/fa-solid-900.e245d7d6.woff2"
+  }
+]);
